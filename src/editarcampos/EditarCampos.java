fba907f3f45a3f0c799b4960a778d35e0556a4af
Diego <Diego@Diego-PC>
2018-11-07 21:15:10 -0200

    Commit madman

--- src/editarcampos/EditarCampos.java
+++ src/editarcampos/EditarCampos.java
@@ -16,6 +16,10 @@ public class EditarCampos {
      */
     public static void main(String[] args) {
         System.out.println("editado");
+        System.out.println("editado");
+        System.out.println("editado");
+        System.out.println("editado");
+        
     }
     
 }
